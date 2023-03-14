bfcb2a4241f5d8da1e1bdbf14bae67fe08b298b6
krisna <krisnasirait48@gmail.com>
2023-03-14 15:47:07 +0700

    working rv with dummy data

--- app/src/main/java/com/krisna/gitbuddy/MainActivity.kt
+++ app/src/main/java/com/krisna/gitbuddy/MainActivity.kt
@@ -2,20 +2,34 @@ package com.krisna.gitbuddy
 
 import android.app.SearchManager
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.krisna.gitbuddy.adapter.UserAdapter
+import com.krisna.gitbuddy.data.DataUser
 import com.krisna.gitbuddy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var adapterUser: UserAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        adapterUser = UserAdapter()
+        adapterUser.setDataUser(DataUser.itemUser)
+        binding.rvUser.apply {
+            layoutManager = LinearLayoutManager(context)
+            setHasFixedSize(true)
+            adapter = adapterUser
+        }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
