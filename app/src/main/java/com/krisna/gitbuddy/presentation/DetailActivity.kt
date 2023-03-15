5a7af471c8920641df92e85c035131397f48a8c3
krisna <krisnasirait48@gmail.com>
2023-03-15 22:53:20 +0700

    optimize code in detail activity

--- app/src/main/java/com/krisna/gitbuddy/presentation/DetailActivity.kt
+++ app/src/main/java/com/krisna/gitbuddy/presentation/DetailActivity.kt
@@ -1,21 +1,20 @@
 package com.krisna.gitbuddy.presentation
 
 import android.os.Bundle
+import android.os.Parcelable
 import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import androidx.viewpager2.widget.ViewPager2
 import com.bumptech.glide.Glide
 import com.google.android.material.tabs.TabLayoutMediator
 import com.krisna.gitbuddy.R
 import com.krisna.gitbuddy.adapter.SectionsPagerAdapter
 import com.krisna.gitbuddy.data.model.response.alluser.AllUserResponseItem
-import com.krisna.gitbuddy.data.model.response.detail.DetailUserResponse
 import com.krisna.gitbuddy.data.model.response.search.SearchResponseItem
 import com.krisna.gitbuddy.databinding.ActivityDetailBinding
 import com.krisna.gitbuddy.presentation.viewmodel.GithubViewModel
-import kotlinx.android.synthetic.main.activity_detail.*
 
+@Suppress("DEPRECATION")
 class DetailActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDetailBinding
@@ -38,7 +37,7 @@ class DetailActivity : AppCompatActivity() {
         val viewPager = binding.viewpager
         viewPager.adapter = sectionsPagerAdapter
 
-        TabLayoutMediator(tabs, viewPager) { tab, position ->
+        TabLayoutMediator(binding.tabs, viewPager) { tab, position ->
             tab.text = resources.getString(TAB_TITLES[position])
         }.attach()
 
@@ -46,13 +45,15 @@ class DetailActivity : AppCompatActivity() {
 
         githubViewModel = ViewModelProvider(this)[GithubViewModel::class.java]
 
-        intent?.getParcelableExtra<AllUserResponseItem>("clickedUser")?.apply {
-            githubViewModel.getUserDetail(login)
+        val login = intent?.getParcelableExtra<Parcelable>("clickedUser")?.let { clickedUser ->
+            when (clickedUser) {
+                is AllUserResponseItem -> clickedUser.login
+                is SearchResponseItem -> clickedUser.login
+                else -> null
+            }
         }
 
-        intent?.getParcelableExtra<SearchResponseItem>("clickedSearch")?.apply {
-            githubViewModel.getUserDetail(login)
-        }
+        login?.let { githubViewModel.getUserDetail(it) }
 
         githubViewModel.detailUser.observe(this) { data->
             binding.tvName.text = data?.name
