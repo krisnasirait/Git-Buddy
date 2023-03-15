8847368055ee77a5bcea6bd2d921a058260e13ff
krisna <krisnasirait48@gmail.com>
2023-03-15 22:57:39 +0700

    optimize SectionsPagerAdapter.kt

--- app/src/main/java/com/krisna/gitbuddy/adapter/SectionsPagerAdapter.kt
+++ app/src/main/java/com/krisna/gitbuddy/adapter/SectionsPagerAdapter.kt
@@ -7,17 +7,11 @@ import com.krisna.gitbuddy.presentation.fragment.FollowersFragment
 import com.krisna.gitbuddy.presentation.fragment.FollowingFragment
 
 class SectionsPagerAdapter(activity: AppCompatActivity) : FragmentStateAdapter(activity) {
-    override fun getItemCount(): Int {
-        return 2
-    }
-
-    override fun createFragment(position: Int): Fragment {
-        var fragment: Fragment? = null
-        when (position) {
-            0 -> fragment = FollowersFragment()
-            1 -> fragment = FollowingFragment()
-        }
-        return fragment as Fragment
+    override fun getItemCount() = 2
+    override fun createFragment(position: Int) = when (position) {
+        0 -> FollowersFragment()
+        1 -> FollowingFragment()
+        else -> throw IllegalArgumentException("Invalid position: $position")
     }
 
 }
--- app/src/main/java/com/krisna/gitbuddy/presentation/viewmodel/GithubViewModel.kt
+++ app/src/main/java/com/krisna/gitbuddy/presentation/viewmodel/GithubViewModel.kt
@@ -92,4 +92,6 @@ class GithubViewModel: ViewModel() {
             }
         }
     }
+
+
 }
