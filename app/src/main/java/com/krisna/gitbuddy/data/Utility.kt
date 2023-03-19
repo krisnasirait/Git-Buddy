94393b00abb94d396f8a784070edc9ef45eaf790
krisna <krisnasirait48@gmail.com>
2023-03-19 17:37:12 +0700

    move token to a safer method

--- app/src/main/java/com/krisna/gitbuddy/data/Utility.kt
+++
@@ -1,5 +0,0 @@
-package com.krisna.gitbuddy.data
-
-object Utility {
-    const val authToken = "token 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"
-}
--- app/src/main/java/com/krisna/gitbuddy/data/repository/GithubRepository.kt
+++ app/src/main/java/com/krisna/gitbuddy/data/repository/GithubRepository.kt
@@ -1,30 +1,32 @@
 package com.krisna.gitbuddy.data.repository
 
-import com.krisna.gitbuddy.data.Utility
+import com.krisna.gitbuddy.BuildConfig
 import com.krisna.gitbuddy.data.model.response.FollowingResponse
-import com.krisna.gitbuddy.data.model.response.search.SearchResponse
 import com.krisna.gitbuddy.data.model.response.alluser.AllUserResponse
 import com.krisna.gitbuddy.data.model.response.detail.DetailUserResponse
 import com.krisna.gitbuddy.data.model.response.followers.FollowersResponse
+import com.krisna.gitbuddy.data.model.response.search.SearchResponse
 import com.krisna.gitbuddy.data.remote.ApiClient
 
 class GithubRepository {
 
+    private val authToken = BuildConfig.AUTH_TOKEN_KEY
+
     suspend fun getUserList() : AllUserResponse {
-        return ApiClient.instance.getUserList(Utility.authToken)
+        return ApiClient.instance.getUserList(authToken)
     }
     suspend fun searchUser(username: String) : SearchResponse {
-        return ApiClient.instance.searchUser(username, Utility.authToken)
+        return ApiClient.instance.searchUser(username, authToken)
     }
     suspend fun getUserDetail(username: String) : DetailUserResponse {
-        return ApiClient.instance.getUser(username, Utility.authToken)
+        return ApiClient.instance.getUser(username, authToken)
     }
 
     suspend fun getUserFollowers(username: String) : FollowersResponse {
-        return ApiClient.instance.getUserFollowers(username, Utility.authToken)
+        return ApiClient.instance.getUserFollowers(username, authToken)
     }
 
     suspend fun getUserFollowing(username: String) : FollowingResponse {
-        return ApiClient.instance.getUserFollowing(username, Utility.authToken)
+        return ApiClient.instance.getUserFollowing(username, authToken)
     }
 }
